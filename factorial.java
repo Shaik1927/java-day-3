a4395989124d9e9c1f9aa5e117af445ec6a03e11
Shaik1927 <112462557+Shaik1927@users.noreply.github.com>
2022-09-28 10:58:32 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,13 @@
+class Test {
+static int factorial(int n)
+{
+if (n==0)
+return 1;
+return n*factorial(n-1);
+}
+public static void main(String[] args)
+{
+int num=5;
+System.out.println("factorial of "+ num +" is " +factorial(5));
+}
+}
